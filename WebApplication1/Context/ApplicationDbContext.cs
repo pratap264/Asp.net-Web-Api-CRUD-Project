96654f38b595e9060bc705a8365a2ca80e51c87e
pratap264 <pratapsingha.21910846@viit.ac.in>
2023-09-15 11:58:13 +0530

    first commit

---
+++ WebApplication1/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        { 
+        
+        }
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string EmailId { get; set; } = null!;
+
+        public string Department { get; set; } = null!;
+
+        public int Phone { get; set; }
+    }
+}
